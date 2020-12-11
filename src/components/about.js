8ba5a82b126a70cee2292a49e52355d69e5f097b
ni-romero <luz.nicole.maria@gmail.com>
2020-12-11 12:11:14 -0300

    about

---
+++ src/components/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const About = () => {
+    return (
+        <>
+            <h1 className="text-center p-5">Sobre nosotros.</h1>
+
+            <h5 className="text-center p-5">Somos un grupo de amigas luchando por todos los que no tienen voz!!! <br></br>
+                 💪🏻❤️ Pequeña Agrupación sin fines de lucro 🐾 TE INVITAMOS A SER PARTE !!</h5>
+
+        </>
+    )
+}
+export default About;
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,48 +6,65 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
+import About from "./about"
 import Header from "./header"
 import "./layout.css"
+import about from "./about"
+import  'bootstrap/dist/css/bootstrap.min.css' ;
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+
+const Layout = () => {
+
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Orejitas Callejeras</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre Nosotros</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Contacto</a>
+          <a class="dropdown-item" href="#">Donaciones</a>
+         
+        </div>
+      </li>
+    </ul>
+  
+  </div>
+</nav>
+
+
+
+
+    <div className="padre"></div>
+    <div className="portada">
+    <h1 className="tituloportada mt-5 p-5">Aspiramos a un Refugio y necesitamos hogares de tránsito”
+     ayúdennos, <br></br> 
+    Orejitascallejeras somos todos</h1>
+    </div>
+    <About/>
+
+ 
+
+ 
     </>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+
 
 export default Layout
